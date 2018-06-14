49c1b775b2e6cc354085c3353cf7e309f6d0d986
Matheus Pelegrini <kevin.pelegrini@outlook.com>
2018-06-14 20:42:43 -0300

    Endpoint /clientes/id disponível

---
+++ src/main/java/com/pelegrini/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.pelegrini.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pelegrini.domain.Cliente;
+import com.pelegrini.repositories.ClienteRepository;
+import com.pelegrini.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+id+", Tipo: "+Cliente.class.getName()));
+	}
+}
